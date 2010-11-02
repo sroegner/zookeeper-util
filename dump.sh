34c6854605bdbee23a7d835612c246a4b068a1bb
Steffen Roegner <steffen@sroegner.org>
2010-11-02 07:36:01 -0400

    Added jruby runtime and shell script for distribution

---
+++ dump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+JAVA_HOME=${JAVA_HOME:-"/usr/java/latest"}
+JAVA_BIN="${JAVA_HOME}/bin/java"
+
+[ ! -x ${JAVA_BIN} ] && echo "Cannot find the java executable in JAVA_HOME: $JAVA_HOME" && exit 64
+
+cd $(dirname ${0})
+DIST_DIR=$PWD
+cd -
+
+JRUBY_DIST="${DIST_DIR}/libs/jruby-dist/jruby-complete-1.5.3.jar"
+
+${JAVA_BIN} -jar ${JRUBY_DIST} ${DIST_DIR}/zk_dump.rb $*
+
